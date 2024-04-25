568a991627367e64674660f4bd55b6752bb27f3a
alex2772 <alex2772sc@gmail.com>
2024-04-26 02:39:09 +0300

    u

--- toggle.sh
+++ toggle.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 LAMP_FILE_PATH="/tmp/.lamp"
 
 value=`cat $LAMP_FILE_PATH`
 
 if [ "$value" != "0" ]; then
-    expect ./on.sh
+    echo "Enable"
+    expect $SCRIPT_DIR/on.sh
+    sleep 1
+    expect $SCRIPT_DIR/on.sh
+    sleep 1
+    expect $SCRIPT_DIR/on.sh
     echo "0" > $LAMP_FILE_PATH
 else
-    expect ./off.sh
+    echo "Disable"
+    expect $SCRIPT_DIR/off.sh
+    sleep 1
+    expect $SCRIPT_DIR/off.sh
+    sleep 1
+    expect $SCRIPT_DIR/off.sh
+    sleep 1
+    expect $SCRIPT_DIR/off.sh
     echo "1" > $LAMP_FILE_PATH
 fi
